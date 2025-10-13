69e473927a8b678ece9b6c941459402f40b644da
Hakan Demir <hhkandemir@gmail.com>
2025-10-13 09:40:24 +0300

    6. gun kodları ve odev cozumu

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class GamerManager : IGamerService
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt oldu");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi");
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿--Select
+select ContactName, CompanyName, City from Customers;
+
+select * from Customers where City = 'London';
+
+-- case insensitive
+
+sElECt *from Products where categoryId=1 or categoryId=3;
+
+select * from Products where categoryId=1 and UnitPrice >= 10;
+
+select * from Products order by ProductName;
+
+select CategoryId , count(*) from Products group by CategoryId;
